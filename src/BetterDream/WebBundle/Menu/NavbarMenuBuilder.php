078293b7b7de66a981e55fdf930449f3134d5e52
Adrien BRAULT <monsti@gmail.com>
2012-05-16 17:53:17 +0200

    Add a language switcher in the menu

--- src/BetterDream/WebBundle/Menu/NavbarMenuBuilder.php
+++ src/BetterDream/WebBundle/Menu/NavbarMenuBuilder.php
@@ -21,14 +21,20 @@ class NavbarMenuBuilder extends AbstractNavbarMenuBuilder
     protected $translator;
 
     /**
+     * @var array
+     */
+    protected $locales;
+
+    /**
      * @param FactoryInterface    $factory
      * @param TranslatorInterface $translator
      */
-    public function __construct(FactoryInterface $factory, TranslatorInterface $translator)
+    public function __construct(FactoryInterface $factory, TranslatorInterface $translator, array $locales)
     {
         parent::__construct($factory);
 
         $this->translator = $translator;
+        $this->locales = $locales;
     }
 
     /**
@@ -44,6 +50,34 @@ class NavbarMenuBuilder extends AbstractNavbarMenuBuilder
         $menu->addChild($this->translator->trans('titles.help'), array('route' => 'betterdream_web_main_help'));
         $menu->addChild($this->translator->trans('titles.contact'), array('route' => 'betterdream_web_main_contact'));
         $menu->addChild($this->translator->trans('titles.about'), array('route' => 'betterdream_web_main_about'));
+
+        return $menu;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function createRightMenu(Request $request)
+    {
+        $menu = $this->factory->createItem('root');
+        $menu->setChildrenAttribute('class', 'nav pull-right');
+
+        $dropDownLabel = $this->translator->trans('language.choice') . ' <b class="caret"></b>';
+        $dropDown = $this->createDropdownMenuItem($menu, $dropDownLabel);
+        $dropDown->setExtra('safe_label', true);
+
+        foreach ($this->locales as $locale) {
+            $icon = sprintf('<i class="icon-%s"></i>', $this->translator->getLocale() == $locale ? 'check' : '');
+            $localeString = $icon . ' ' . $this->translator->trans(sprintf('locales.%s', $locale));
+
+            $child = $dropDown->addChild($localeString, array(
+                'route' => $request->attributes->get('_route'),
+                'routeParameters' => array(
+                    '_locale' => $locale,
+                ),
+            ));
+            $child->setExtra('safe_label', true);
+        }
 
         return $menu;
     }
